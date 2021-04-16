3cc5cae0610933b1fc5febbbf14c3724d9beefe2
Roman Heinrich <roman.heinrich@gmail.com>
2021-04-16 22:37:01 +0200

    Simplify jest config

--- templates/template-vanilla-ts/jest.config.js
+++ templates/template-vanilla-ts/jest.config.js
@@ -1,11 +1,11 @@
-// jest.config.js
+const sharedConfig = require("../../jest.config.js");
+let moduleNameMapper = {
+  "\\.(css|less|sass|scss)$": "<rootDir>/../../__mocks__/styleMock.js",
+  "\\.(gif|ttf|eot|svg|wav)$": "<rootDir>/../../__mocks__/fileMock.js",
+};
+
 module.exports = {
-  verbose: true,
-  transform: {
-    "^.+\\.tsx?$": "esbuild-jest",
-  },
-  moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
-    "\\.(gif|ttf|eot|svg|wav)$": "<rootDir>/__mocks__/fileMock.js",
-  },
+  ...sharedConfig,
+  rootDir: "./",
+  moduleNameMapper: moduleNameMapper,
 };
